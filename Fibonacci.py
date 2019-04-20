b4fec0f92fa2b9fd995ada8d86775dc070bb4bb6
Hitomamacs <macsdeve@gmail.com>
2019-04-20 22:11:17 +0200

    ciao

--- Fibonacci.py
+++ Fibonacci.py
@@ -4,9 +4,12 @@
 def fibo(n):
     if n == 0:
         return 1
+
     elif n == 1:
         return 1
     elif n >= 2:
         return fibo(n-1)+fibo(n-2)
 for x in range(int(input('quanti numeri della sequenza vuoi avere?'))):
     print(fibo(x))
+
+ciao
